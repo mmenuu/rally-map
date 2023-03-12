c624d86f16846d458fc3fd8e8be772b2e7922cb1
1tpp <panapat.dev@gmail.com>
2023-03-12 15:41:37 +0700

    chore: change tile map to openstreetmap

--- src/App.jsx
+++ src/App.jsx
@@ -137,8 +137,8 @@ function App() {
         placeholder={<MapPlaceholder />}
       >
         <TileLayer
-          attribution='Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>'
-          url={`https://api.mapbox.com/styles/v1/${import.meta.env.VITE_MAPBOX_USERNAME}/${import.meta.env.VITE_MAPBOX_STYLE}/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_TOKEN}`}
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
         />
         <LoadNodes />
         {nodes.length > 0 && nodes.map((node) => (
