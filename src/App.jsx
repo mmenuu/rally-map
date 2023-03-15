05777ddb8176a38bb951a0488b13e9fa54abe370
1tpp <panapat.dev@gmail.com>
2023-03-15 23:16:43 +0700

    feat: calculate distanses between waypoint and waypoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,133 @@
-import { useState, useRef } from 'react'
-import RoutingControl from './RoutingControl';
-import debounce from 'lodash/debounce';
+import { useState, useRef } from "react";
+import RoutingControl from "./RoutingControl";
+import debounce from "lodash/debounce";
 
-import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMapEvents, ZoomControl } from 'react-leaflet'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvent,
+  ZoomControl,
+} from "react-leaflet";
 import axios from "axios";
-import './App.css'
+import "./App.css";
 
 function SetViewOnClick({ animateRef }) {
-  const map = useMapEvent('click', (e) => {
+  const map = useMapEvent("click", (e) => {
     map.setView(e.latlng, map.getZoom(), {
       animate: animateRef.current || false,
-    })
-  })
+    });
+  });
 
-  return null
+  return null;
 }
 
 function MapPlaceholder() {
   return (
     <p>
-      Rally Map.{' '}
+      Rally Map.{" "}
       <noscript>You need to enable JavaScript to see this map.</noscript>
     </p>
-  )
+  );
 }
 
 function App() {
-  const animateRef = useRef(true)
+  const animateRef = useRef(true);
   const [nodes, setNodes] = useState([]);
   const [route, setRoute] = useState([]);
   const [navigate, setNavigate] = useState(false);
+  const [routingDetails, setRoutingDetails] = useState({
+    totalDistance: null,
+    totalTime: null,
+    distanceBetweenWaypoints: [],
+  });
 
   const searchNodes = async ({ lat, lng }) => {
-    const url = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:3000,${lat}, ${lng})[%22amenity%22=%22restaurant%22];out;`;
+    const url = `${
+      import.meta.env.VITE_MAP_DATA_API
+    }?data=[out:json];node(around:3000,${lat}, ${lng})[%22amenity%22=%22restaurant%22];out;`;
     const response = await axios.get(url);
     const data = response.data;
-    const nodes = data.elements.map(element => ({
+    const nodes = data.elements.map((element) => ({
       id: element.id,
       position: [element.lat, element.lon],
-      name: element.tags.name ? element.tags.name : 'N/A',
+      name: element.tags.name ? element.tags.name : "N/A",
       amenity: element.tags.amenity,
-      opening_hours: element.tags.opening_hours ? element.tags.opening_hours : 'N/A',
+      opening_hours: element.tags.opening_hours
+        ? element.tags.opening_hours
+        : "N/A",
     }));
     setNodes(nodes);
   };
 
   function LoadNodes({ searchNodes }) {
-    const map = useMapEvent('moveend', debounce(() => {
-      const center = map.getCenter();
-      searchNodes({
-        lat: center.lat,
-        lng: center.lng,
-      });
-    }, 1000));
+    const map = useMapEvent(
+      "moveend",
+      debounce(() => {
+        const center = map.getCenter();
+        searchNodes({
+          lat: center.lat,
+          lng: center.lng,
+        });
+      }, 1000)
+    );
 
     return null;
   }
 
   function addNodeToRoute(node) {
     setRoute([...route, node]);
-    handleRouting()
+    handleNewRouting();
   }
 
   function removeNodeFromRoute(node) {
-    setRoute(route.filter(n => n.id !== node.id));
-    handleRouting()
+    setRoute(route.filter((n) => n.id !== node.id));
+    handleNewRouting();
   }
 
-  function handleRouting() {
+  function handleNewRouting() {
     setNavigate(false);
     setTimeout(() => {
       setNavigate(true);
     }, 1);
   }
 
+  function handleRouting(details) {
+    setRoutingDetails(details);
+  }
+
   function nodeNotInRoute(node) {
-    return route.find(n => n.id === node.id) === undefined;
+    return route.find((n) => n.id === node.id) === undefined;
   }
 
   return (
-    <div className='relative w-screen h-screen'>
+    <div className="relative w-screen h-screen">
       {route.length > 0 && (
-        <div className='absolute bg-white z-20 p-4 w-[325px] md:left-2 md:top-2 max-sm:left-0 max-sm:right-0 rounded-xl shadow-2xl max-h-[40vh] overflow-scroll'>
-          <h1 className='text-3xl text-center'>Roadtrip</h1>
-          <h3 className='text-xl text-center mb-4 text-gray-400'>
-            Waypoints
-          </h3>
+        <div className="absolute bg-white z-20 p-4 w-[325px] md:left-2 md:top-6 max-sm:left-0 max-sm:right-0 rounded-xl shadow-2xl h-[80vh] overflow-scroll">
+          <h1 className="text-3xl text-center">Roadtrip</h1>
+          <h3 className="text-xl text-center mb-4 text-gray-400">Waypoints</h3>
 
-          <ul
-            className='flex flex-col space-y-2 list-none'
-          >
+          <ul className="flex flex-col list-none">
             {route.map((node, index) => (
-              <li
-                className='rounded-md p-3 bg-gray-50 shadow-md cursor-grab'
-                key={node.id}
-              >
-                <div className='flex justify-between items-center'>
-                  <div className='flex flex-col'>
-                    <div className='flex justify-between items-center'>
-                      <span
-                        className='text-2xl font-semibold text-gray-400 mr-4'
-                      >{index + 1}</span>
+              <li className="cursor-grab" key={node.id}>
+                <div className="flex p-3 bg-gray-50 rounded-sm  justify-between items-center">
+                  <div className="flex flex-col">
+                    <div className="flex justify-between items-center">
+                      <span className="text-2xl font-semibold text-gray-400 mr-4">
+                        {index + 1}
+                      </span>
 
-                      <div className='flex flex-col'>
-                        <h2
-                          className='text-xl font-medium'
-                        >{node.name}</h2>
-                        <span
-                          className='text-sm text-gray-400 capitalize'
-                        >{node.amenity}</span>
+                      <div className="flex flex-col">
+                        <h2 className="text-xl font-medium">{node.name}</h2>
+                        <span className="text-sm text-gray-400 capitalize">
+                          {node.amenity}
+                        </span>
                       </div>
                     </div>
-
                   </div>
-                  <button className='cursor-pointer'
+                  <button
+                    className="cursor-pointer"
                     onClick={() => removeNodeFromRoute(node)}
                   >
                     <svg
@@ -132,32 +146,52 @@ function App() {
                     </svg>
                   </button>
                 </div>
+                {route.length > 1 && route.length - 1 !== index && (
+                  <div className="flex items-center my-4">
+                    <div className="border-b w-full"></div>
+                    <span className="w-24 ml-5 font-medium text-gray-400">
+                      {routingDetails.distanceBetweenWaypoints[index]
+                        ? `${parseFloat(
+                            routingDetails.distanceBetweenWaypoints[index] /
+                              1000
+                          ).toFixed(2)} km`
+                        : "calculating..."}
+                    </span>
+                  </div>
+                )}
+                {route.length - 1 === index && route.length > 1 && (
+                  <p className="items-center my-4 text-md font-semibold text-blue-400 text-center">
+                    Total Distance:{" "}
+                    {parseFloat(routingDetails.totalDistance / 1000).toFixed(2)}{" "}
+                    km
+                  </p>
+                )}
               </li>
             ))}
           </ul>
         </div>
-      )
-      }
-      {
-        route.length > 1 && (
-          <button className="absolute text-green-400 bottom-2 left-2 font-semibold py-2 bg-green-50 px-3 rounded-md hover:bg-green-100 hover:ring-2 hover:ring-green-400 mt-4 z-20"
-            onClick={() => {
-              alert(JSON.stringify(route));
-            }}>
-            Save Trip
-          </button>
-        )
-      }
+      )}
+      {route.length > 1 && (
+        <button
+          className="absolute text-green-400 bottom-2 left-2 font-semibold py-2 bg-green-50 px-3 rounded-md hover:bg-green-100 hover:ring-2 hover:ring-green-400 mt-4 z-20"
+          onClick={() => {
+            alert(JSON.stringify(route));
+          }}
+        >
+          Save Trip
+        </button>
+      )}
       <MapContainer
-        className='z-10'
+        className="z-10"
         center={[13.7294053, 100.7758304]}
         zoom={13}
         zoomControl={false}
-        whenReady={() => searchNodes({
-          lat: 13.7294053,
-          lng: 100.7758304,
-        })}
-
+        whenReady={() =>
+          searchNodes({
+            lat: 13.7294053,
+            lng: 100.7758304,
+          })
+        }
         scrollWheelZoom={true}
         placeholder={<MapPlaceholder />}
       >
@@ -166,59 +200,58 @@ function App() {
           url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
         />
         <LoadNodes searchNodes={searchNodes} />
-        {nodes.length > 0 && nodes.map((node) => (
-          <Marker key={node.id} position={node.position}>
-            <Popup>
-              <div
-                className='flex flex-col flex-wrap space-y-1 justify-between min-w-[225px]'
-              >
-                <h1 className='text-2xl font-medium'>{
-                  node.name
-                }</h1>
-                <div className='flex flex-wrap justify-between items-center pb-3'>
-                  <span className='text-yellow-400 bg-yellow-50 px-3 py-1 rounded-md capitalize'>{node.amenity}</span>
-                  <time className='text-gray-400'>
-                    {node.opening_hours}
-                  </time>
-                </div>
-                {
-                  route.length === 0 ? (
+        {nodes.length > 0 &&
+          nodes.map((node) => (
+            <Marker key={node.id} position={node.position}>
+              <Popup>
+                <div className="flex flex-col flex-wrap space-y-1 justify-between min-w-[225px]">
+                  <h1 className="text-2xl font-medium">{node.name}</h1>
+                  <div className="flex flex-wrap justify-between items-center pb-3">
+                    <span className="text-yellow-400 bg-yellow-50 px-3 py-1 rounded-md capitalize">
+                      {node.amenity}
+                    </span>
+                    <time className="text-gray-400">{node.opening_hours}</time>
+                  </div>
+                  {route.length === 0 ? (
                     <button
-                      className='text-green-400 font-semibold py-2 bg-green-50 px-3 rounded-md hover:bg-green-100 hover:ring-2 hover:ring-green-400 w-full capitalize'
+                      className="text-green-400 font-semibold py-2 bg-green-50 px-3 rounded-md hover:bg-green-100 hover:ring-2 hover:ring-green-400 w-full capitalize"
                       onClick={() => addNodeToRoute(node)}
                     >
                       Start New Trip
-                    </button>) :
-                    nodeNotInRoute(node) ? (
-                      <button
-                        className='text-blue-400 font-semibold py-2 bg-blue-50 px-3 rounded-md hover:bg-blue-100 hover:ring-2 hover:ring-blue-400 w-full capitalize'
-                        onClick={() => addNodeToRoute(node)}
-                      >
-                        Add to trip
-                      </button>
-                    ) : (
-                      <button
-                        className='text-red-400 font-semibold py-2 bg-red-50 px-3 rounded-md hover:bg-red-100 hover:ring-2 hover:ring-red-400 w-full capitalize'
-                        onClick={() => removeNodeFromRoute(node)}
-                      >
-                        Remove from trip
-                      </button>
-                    )
-                }
-              </div>
-            </Popup>
-          </Marker>
-        ))}
+                    </button>
+                  ) : nodeNotInRoute(node) ? (
+                    <button
+                      className="text-blue-400 font-semibold py-2 bg-blue-50 px-3 rounded-md hover:bg-blue-100 hover:ring-2 hover:ring-blue-400 w-full capitalize"
+                      onClick={() => addNodeToRoute(node)}
+                    >
+                      Add to trip
+                    </button>
+                  ) : (
+                    <button
+                      className="text-red-400 font-semibold py-2 bg-red-50 px-3 rounded-md hover:bg-red-100 hover:ring-2 hover:ring-red-400 w-full capitalize"
+                      onClick={() => removeNodeFromRoute(node)}
+                    >
+                      Remove from trip
+                    </button>
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          ))}
         <SetViewOnClick animateRef={animateRef} />
 
         {navigate && (
-          <RoutingControl position="bottomright" color="red" waypoints={route.map(node => node.position)} />
-        )
-        }
+          <RoutingControl
+            position="bottomright"
+            color="#856be3"
+            waypoints={route.map((node) => node.position)}
+            onRouting={handleRouting}
+          />
+        )}
         <ZoomControl position="topright" />
       </MapContainer>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default App;
--- src/RoutingControl.jsx
+++ src/RoutingControl.jsx
@@ -3,8 +3,12 @@ import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 
-const createRoutineMachineLayer = ({ position, waypoints, color }) => {
-
+const createRoutineMachineLayer = ({
+  position,
+  waypoints,
+  color,
+  onRouting,
+}) => {
   const instance = L.Routing.control({
     position,
     waypointMode: "snap",
@@ -14,6 +18,7 @@ const createRoutineMachineLayer = ({ position, waypoints, color }) => {
       styles: [
         {
           color,
+          weight: 5,
         },
       ],
     },
@@ -24,9 +29,25 @@ const createRoutineMachineLayer = ({ position, waypoints, color }) => {
 
   instance.on("routesfound", (e) => {
     const { routes } = e;
-    const { summary } = routes[0];
+    const { summary, instructions } = routes[0];
     const { totalDistance, totalTime } = summary;
-    console.log(totalDistance / 1000,Math.round(totalTime % 3600 / 60));
+
+    const distances = instructions.map((instruction) => {
+      const { distance } = instruction;
+      return distance;
+    });
+    
+    let distanceBetweenWaypoints = [];
+    let distanceBetweenWaypointsTotal = 0;
+    for (let i = 0, len = distances.length; i < len; i++) {
+      distanceBetweenWaypointsTotal += distances[i];
+      if (distances[i] === 0) {
+        distanceBetweenWaypoints.push(distanceBetweenWaypointsTotal);
+        distanceBetweenWaypointsTotal = 0;
+      }
+    }
+
+    onRouting({ totalDistance, totalTime, distanceBetweenWaypoints });
   });
 
   return instance;
