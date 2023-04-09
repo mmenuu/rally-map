976721488e0d13fd54a179f6c2c003a251247e18
1tpp <panapat.dev@gmail.com>
2023-04-10 04:25:41 +0700

    chore: remove unuse import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useCallback } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { AuthProvider, useAuth } from "./context/authContext";
