78ceae273e0b297ac73cfcd5330d83b5c1f030ad
1tpp <panapat.dev@gmail.com>
2023-04-12 04:24:01 +0700

    fix: jsx syntax

--- src/routes/map.jsx
+++ src/routes/map.jsx
@@ -200,8 +200,6 @@ function MapPage() {
   };
 
   useEffect(() => {
-    getFavorites();
-
     if (roadtrip.id !== null) {
       handleUpdateTrip();
     }
@@ -218,6 +216,10 @@ function MapPage() {
     }
   }, [roadtrip]);
 
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
   return (
     <div className="relative w-screen h-screen z-0">
       {roadtrip.waypoints.length > 0 && (
@@ -230,12 +232,12 @@ function MapPage() {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
                   <span className="text-sm">{roadtrip.waypoints.length}</span>
@@ -246,12 +248,12 @@ function MapPage() {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
 
@@ -264,15 +266,14 @@ function MapPage() {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path d="M6.5 3c-1.051 0-2.093.04-3.125.117A1.49 1.49 0 002 4.607V10.5h9V4.606c0-.771-.59-1.43-1.375-1.489A41.568 41.568 0 006.5 3zM2 12v2.5A1.5 1.5 0 003.5 16h.041a3 3 0 015.918 0h.791a.75.75 0 00.75-.75V12H2z" />
                     <path d="M6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM13.25 5a.75.75 0 00-.75.75v8.514a3.001 3.001 0 014.893 1.44c.37-.275.61-.719.595-1.227a24.905 24.905 0 00-1.784-8.549A1.486 1.486 0 0014.823 5H13.25zM14.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                   </svg>
 
                   <span className="text-sm">
-                    {parseFloat(roadtrip.total_distance / 1000).toFixed(3)}{" "}
-                    km
+                    {parseFloat(roadtrip.total_distance / 1000).toFixed(3)} km
                   </span>
                 </div>
 
@@ -285,7 +286,7 @@ function MapPage() {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-6 text-gray-300 hover:text-gray-400"
+                  className="w-6 h-6 text-gray-300 hover:text-gray-400"
                 >
                   <path
                     strokeLinecap="round"
@@ -300,8 +301,7 @@ function MapPage() {
               {roadtrip.waypoints.length > 1 && (
                 <p className="items-center mb-4 text-md font-semibold text-blue-400 text-center">
                   Total Distance:{" "}
-                  {parseFloat(roadtrip.total_distance / 1000).toFixed(3)}{" "}
-                  km
+                  {parseFloat(roadtrip.total_distance / 1000).toFixed(3)} km
                 </p>
               )}
               <WaypointList
@@ -374,7 +374,7 @@ function MapPage() {
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
                           fill="currentColor"
-                          class="w-6 h-6 text-red-500"
+                          className="w-6 h-6 text-red-500"
                         >
                           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                         </svg>
@@ -392,7 +392,7 @@ function MapPage() {
                           viewBox="0 0 24 24"
                           strokeWidth="1.5"
                           stroke="currentColor"
-                          class="w-6 h-6"
+                          className="w-6 h-6"
                         >
                           <path
                             strokeLinecap="round"
