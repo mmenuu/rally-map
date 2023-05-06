1cbb686c4b7c68a4e30c0a3bb113ca307b34a0ab
1tpp <panapat.dev@gmail.com>
2023-05-06 15:49:43 +0700

    feat: class to function component

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import CLOUDS from "vanta/dist/vanta.clouds.min.js";
-
 import { Link } from "react-router-dom";
 
-class HomePage extends React.Component {
-  constructor() {
-    super();
-    this.vantaRef = React.createRef();
-  }
-  componentDidMount() {
-    this.vantaEffect = CLOUDS({
-      el: this.vantaRef.current,
+const HomePage = () => {
+  const vantaRef = useRef(null);
+  let vantaEffect = null;
+
+  useEffect(() => {
+    vantaEffect = CLOUDS({
+      el: vantaRef.current,
       THREE: THREE,
       mouseControls: true,
       touchControls: true,
@@ -19,36 +17,36 @@ class HomePage extends React.Component {
       minHeight: 200.0,
       minWidth: 200.0,
     });
-  }
-  componentWillUnmount() {
-    if (this.vantaEffect) {
-      this.vantaEffect.destroy();
-    }
-  }
-  render() {
-    return (
-      <div style={{ height: "100vh", width: "100%" }} ref={this.vantaRef}>
-        <main className="h-screen grid justify-center items-center md:grid-cols-12 container mx-auto px-8 gap-x-10 md:place-items-center pb-10 lg:pb-0 min-h-[42.5rem]">
-          <div className="md:col-span-6 text-white my-14 lg:my-0">
-            <h1 className="capitalize text-6xl lg:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600 ">
-              Rally
-            </h1>
-            <p className="mt-4 mb-10 max-w-lg leading-relaxed text-neutral-100 font-light">
-              Create custom itineraries, discover new routes, find interesting
-              landmarks, and get directions to your destinations - all in one
-              place.
-            </p>
-            <Link
-              to="/map"
-              className="inline-block capitalize font-medium text-lg bg-gradient-to-r from-yellow-400 to-pink-600 text-white px-8 py-2 rounded-full hover:from-yellow-500 hover:to-pink-700 hover:ring-2 hover:ring-yellow-400 hover:ring-offset-2 hover:ring-offset-white"
-            >
-              try it out
-            </Link>
-          </div>
-        </main>
-      </div>
-    );
-  }
-}
+
+    return () => {
+      if (vantaEffect) {
+        vantaEffect.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }} ref={vantaRef}>
+      <main className="h-screen grid justify-center items-center md:grid-cols-12 container mx-auto px-8 gap-x-10 md:place-items-center pb-10 lg:pb-0 min-h-[42.5rem]">
+        <div className="md:col-span-6 text-white my-14 lg:my-0">
+          <h1 className="capitalize text-6xl lg:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600 ">
+            Rally
+          </h1>
+          <p className="mt-4 mb-10 max-w-lg leading-relaxed text-neutral-100 font-light">
+            Create custom itineraries, discover new routes, find interesting
+            landmarks, and get directions to your destinations - all in one
+            place.
+          </p>
+          <Link
+            to="/map"
+            className="inline-block capitalize font-medium text-lg bg-gradient-to-r from-yellow-400 to-pink-600 text-white px-8 py-2 rounded-full hover:from-yellow-500 hover:to-pink-700 hover:ring-2 hover:ring-yellow-400 hover:ring-offset-2 hover:ring-offset-white"
+          >
+            try it out
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default HomePage;
